de74b8fa785ac19db89664c7a1fcc6ed60baff50
Vadimkandeev <vadimkandeev@gmail.com>
2023-03-20 18:24:01 +0400

    ПОследнее обноврение

--- test/conftest.py
+++ test/conftest.py
@@ -6,6 +6,9 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from api.BoardApi import BoardApi
 
+base_url = "https://trello.com/1/"
+token = "62a01eb2f072a11c2e65969c/ATTS2gQtUeAdkLAMAoxtiaMh7Lk9hwnziuABBiVn9MpBMnHufo0MwiF3TW4BAfcq0KbF815A9D09"
+
 @pytest.fixture
 def browser():
     with allure.step("Открыть и настроить браузер"):
@@ -20,19 +23,17 @@ def browser():
 
 @pytest.fixture
 def api_client() -> BoardApi:
-    return BoardApi("https://trello.com/1/", "62a01eb2f072a11c2e65969c/ATTS2gQtUeAdkLAMAoxt\
-                    iaMh7Lk9hwnziuABBiVn9MpBMnHufo0MwiF3TW4BAfcq0KbF815A9D09")  
+    return BoardApi(base_url, token)  
 
 
 @pytest.fixture
 def api_client_no_auth() -> BoardApi:
-    return BoardApi("https://trello.com/1/", "")  
+    return BoardApi(base_url, "")  
 
 
 @pytest.fixture
 def dummy_board_id() -> str:
-    api = BoardApi("https://trello.com/1/", "62a01eb2f072a11c2e65969c/ATTS2gQtUeAdkLAMAoxt\
-                    iaMh7Lk9hwnziuABBiVn9MpBMnHufo0MwiF3TW4BAfcq0KbF815A9D09")
+    api = BoardApi(base_url, token)
     resp = api.create_board("board_for_deleted").get("id")
     return resp
 
@@ -42,8 +43,7 @@ def delete_board() -> str:
     dictionary = {"board_id": ""}
     yield dictionary
 
-    api = BoardApi("https://trello.com/1/", "62a01eb2f072a11c2e65969c/ATTS2gQtUeAdkLAMAoxt\
-                    iaMh7Lk9hwnziuABBiVn9MpBMnHufo0MwiF3TW4BAfcq0KbF815A9D09")
+    api = BoardApi(base_url, token)
     api.delete_board_by_id(dictionary.get("board_id"))
     
 
